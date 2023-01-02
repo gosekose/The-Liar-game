e70cdb7d35595fabf991f8a445106d8cafbb921e
gosekose <ksyn1611@naver.com>
2023-01-02 15:45:03 +0900

    feat: 환경 설정 version: SpringBoot 2.7.7, Java 11 dependencies: DevTools, Web, Lombok, Jpa, Security, Thymeleaf, H2, MySqL, Data Redis IDE: Intellij

---
+++ src/test/java/liar/game/GameApplicationTests.java
@@ -0,0 +1,13 @@
+package liar.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
